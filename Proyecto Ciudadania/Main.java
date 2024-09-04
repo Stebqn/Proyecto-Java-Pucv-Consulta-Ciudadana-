28e65247be6246e2fde77d1bc78191bafc31beb5
Esteban Martinez <esteban.martinez.n@mail.pucv.cl>
2024-09-03 21:03:56 -0400

    Update Main.java

--- Proyecto Ciudadania/Main.java
+++ Proyecto Ciudadania/Main.java
@@ -1,38 +1,12 @@
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.io.*;
+import java.lang.*;
+
+
+
 
 
 public class Main {
-
-    private static BufferedReader lector = new BufferedReader(new InputStreamReader(System.in));
-    
     public static void main(String[] args) throws IOException{
        
-        HashMap<String, Consulta> Consulta = new HashMap<String, Consulta>();
-        ArrayList<Voto> votos = new ArrayList<Voto>();
-        
-        menuPrincipal();
-    }
-
-    public static void menuPrincipal() throws IOException{
-        int confirmacion;
-        
-        System.out.println("Bienvenido al sistema de votaciones");
-        System.out.println("Ingrese '1' para ingresar al sistema de votaciones");
-        confirmacion = Integer.parseInt(lector.readLine());
-        
-        switch(confirmacion) {
-            case 1:
-                System.out.println("Hello world!");
-                break;
-            case 0:
-                System.out.println("Gracias por su visita!");
-                break;
-            default:
-                System.out.println("Opción inválida.");
-                menuPrincipal();
-                break;
-        }
     }
 }
