64bdbcbe07dba25f58bce3d7f73d7218b186811a
Esteban Martinez <esteban.martinez.n@mail.pucv.cl>
2024-09-04 20:35:25 -0400

    Update and rename Main.java to Sistema.java
    Cambio de nombre a Sistema

--- Proyecto Ciudadania/Sistema.java
+++ Proyecto Ciudadania/Sistema.java
@@ -2,11 +2,8 @@ import java.io.*;
 import java.lang.*;
 
 
-
-
-
-public class Main {
+public class Sistema{
     public static void main(String[] args) throws IOException{
-       
-    }
+    
+    } 
 }
