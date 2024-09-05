6d720216316d758a5898f24a2e510ba0eba18b96
Esteban Martinez <esteban.martinez.n@mail.pucv.cl>
2024-09-04 20:34:37 -0400

    Update Consulta.java
    Se agrego funcion listar y funcion agregar

--- Proyecto Ciudadania/Consulta.java
+++ Proyecto Ciudadania/Consulta.java
@@ -17,11 +17,7 @@ public class Consulta {
         this.cantDeVotos = cantDeVotos;
         votos = new HashMap<String,Voto>();
     }
-   
-
-
-
-   
+     
     //=========== Getters ==============\\
     public int getId(){
         return id;
@@ -68,17 +64,45 @@ public class Consulta {
     
 
 
-    public void agregarVoto(Votos voto){
-        votos.put(voto.getId(), voto);
-        cantDeVotos++;
+    public boolean verificarVoto(Voto voto){
+        Voto votoAnterior = votos.put(voto.getRut(), voto);
 
-
+        if(votoAnterior == null){
+            cantDeVotos++;
+            return true;
+        }
+        return false;
     }
+
+    public boolean agregarVoto(int id, String rut, boolean veredicto){
+        Voto nuevoVoto = new Voto();
+        nuevoVoto.setId(id);
+        nuevoVoto.setRut(rut);
+        nuevoVoto.setveredicto(veredicto);
+        
+        boolean agregado = verificarVoto(nuevoVoto);
+
+        if(agregado == true){
+            return true;
+        }
+        return false;
+    }
+
+
 
     // Proximamente eliminar //
 
-    public Votos obtenerVoto(String idVoto) {
-        return votos.get(idVoto);
+    public Voto obtenerVoto(int idVoto) {
+        Voto votoRevision = new Voto();
+        votoRevision.setId(idVoto);
+        
+        return votos.get(String.valueOf(idVoto)); 
+    }
+
+    public Voto obtenerVoto(String rut){
+        Voto votoRevision = new Voto();
+        votoRevision.setRut(rut);
+        return votos.get(String.valueOf(rut));
     }
 
 
@@ -97,10 +121,32 @@ public class Consulta {
         }
         else{
             System.out.println("Votos:");
-            
-
+    
         }
 
+    }
+
+    public void listarVotos() {
+        if (votos.isEmpty()) {
+            System.out.println("No hay votos registrados.");
+        } else {
+            for (String key : votos.keySet()) {  
+                Voto voto = votos.get(key);       
+                
+                // Imprimiendo la salida en el formato que solicitas
+                System.out.println("Id Voto : " + voto.getId());
+                System.out.println("Rut Votante : " + voto.getRut());
+                
+                // Usando if-else para determinar el veredicto
+                if (voto.getveredicto()) {
+                    System.out.println("Veredicto : Apruebo");
+                } else {
+                    System.out.println("Veredicto : Rechazo");
+                }
+
+                System.out.println();  
+            }
+        }
     }
     
 }
