458482f18338079b6dfd7331af82befe38383b1c
Esteban Martinez <esteban.martinez.n@mail.pucv.cl>
2024-09-04 20:37:14 -0400

    Update Menu.java
    Se agrego prints para interactuar con el usuario

--- Proyecto Ciudadania/Menu.java
+++ Proyecto Ciudadania/Menu.java
@@ -28,31 +28,46 @@ public class Menu {
     public static void menuPrincipal(){
         int opcion;
         Scanner escaner = new Scanner(System.in);
-
+        
+        
         System.out.println("Bienvenido se encuentra en el menu de consultas profesional");
         System.out.println("¿Que desea realizar?\n");
 
-        System.out.println("1.Agregar un voto");
-        System.out.println("2.Insertar un voto"); 
-        System.out.println("3.Listado de votos"); 
+        System.out.println("1.Crear un voto");
+        System.out.println("2.Listado de votos"); 
 
         opcion = escaner.nextInt();
         do{ 
             switch(opcion){
                 case 1:
-                    
-                    System.out.println("Agregacion exitosa!");   
+                    int idVotoNuevo;
+                    String RutNuevo;
+                    boolean veredictoNuevo;
+                    boolean resultadoAgregacion;
+                    Voto votoNuevo = new Voto();
+
+                    System.out.println("Ingrese el id del nuevo voto");
+                    idVotoNuevo = escaner.nextInt();
+                    escaner.nextLine();
+                    System.out.println("Ingrese el rut del votante");
+                    RutNuevo = escaner.nextLine();
+                    System.out.println("Ingrese el veredicto del votante");
+                    veredictoNuevo = escaner.nextBoolean();
+                    escaner.nextLine();
+
+                    //resultadoAgregacion = votoNuevo.agregarVoto(idVotoNuevo,RutNuevo,veredictoNuevo);
+
+                    //if(resultadoAgregacion){
+                       //System.out.println("Agregacion exitosa!"); 
+                    //}
+
+                    System.out.println("Lo sentimos, su intento ha fallado");   
                     break;
                 case 2:
-                    System.out.println("Insertar exitoso!");
-                    break;
-                case 3:
                     System.out.println("Listado de votos terminado!");
                     break;
             }
         }while(opcion >= 0);
-
-
         return;
     }
     
