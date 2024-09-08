5674855e542714c51b87adba12837b2d2158f369
Esteban Martinez <esteban.martinez.n@mail.pucv.cl>
2024-09-07 23:31:27 -0400

    Update Sistema.java
    Se agrego una consulta mas y dos objetos votos a la nueva consulta

--- Proyecto Ciudadania/Sistema.java
+++ Proyecto Ciudadania/Sistema.java
@@ -9,11 +9,17 @@ public class Sistema {
 
         ArrayList<Consulta> consultas = new ArrayList<Consulta>();
         Consulta consulta1 = new Consulta(1, "Consulta San Felipe", "10/09/98");
+        Consulta consulta2 = new Consulta(2, "Consulta San Joaquin", "10/09/98");
+
 
         consulta1.agregarVoto(1, "123456789-0", true);
         consulta1.agregarVoto(2, "987654321-0", false);
         consulta1.agregarVoto(3, "111111111-1", false);
+
+        consulta2.agregarVoto(1, "111111222-0", true);
+        consulta2.agregarVoto(2, "222111333-9", false);
         consultas.add(consulta1);
+        consultas.add(consulta2);
 
         menuDeInicio(lector, consultas);
 
