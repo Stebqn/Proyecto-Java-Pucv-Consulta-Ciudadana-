bb8f9869c0f7f6d3957a7f6bb15b7b2d4eb3da60
Esteban Martinez <esteban.martinez.n@mail.pucv.cl>
2024-09-07 22:48:54 -0400

    Update Voto.java
    Se agrego nuevo builder y se corrigio el anterior y se hizo sobrecarga en el metodo mostrar Voto

--- Proyecto Ciudadania/Voto.java
+++ Proyecto Ciudadania/Voto.java
@@ -9,22 +9,27 @@ public class Voto {
     
     //========BUILDER==============\\
     public Voto(){
+        this.id = 0;
+        this.rut = new String();
+        this.veredicto = false;
+    }
 
+    public Voto (int id, String rut, boolean veredicto){
+        this.id = id;
+        this.rut = rut;
+        this.veredicto = veredicto;
     }
 
     //========GETTER==============
     public int getId() {
         return id;
     }
-    
     public String getRut() {
         return rut;
     }
-
     public Boolean getveredicto() {
         return veredicto;
     }
-
     //==========SETTER===============\\
     public void setRut(String rut) {
         this.rut = rut;
@@ -32,28 +37,56 @@ public class Voto {
     public void setId(int id) {
         this.id = id;
     }
-
     public void setveredicto(Boolean veredicto) {
         this.veredicto = veredicto;
     }
-
     //==========MÉTODOS===============\\
-
-
-
-    public String posicionVoto(boolean pos){
-        if (pos)
+    public String posicionVoto(boolean veredicto){
+        if (veredicto)
             return "A favor";
         else
             return "En contra";
     }
+
     public void mostrarVoto(){
         System.out.print("Ciudadan@ (rut): " + this.getRut() +
                 "\nID: " + this.getId() +
                 "\nPosición: " + posicionVoto(this.veredicto) + "\n\n");
     }
 
-    public void modifVoto(){
+    public void mostrarVoto(String rut) {
+
+        if (this.rut.equals(rut)) {
+            System.out.println("Voto encontrado:");
+            System.out.println("ID: " + id);
+            System.out.println("RUT: " + rut);
+            if (veredicto) {
+                System.out.println("Veredicto: A favor");
+            } else {
+                System.out.println("Veredicto: En contra");
+            }
+        } else {
+            System.out.println("Voto no encontrado para el RUT: " + rut);
+        }
+    }
+
+    public void mostrarVoto(int id) {
+
+        if (this.id == id) {
+            System.out.println("Voto encontrado:");
+            System.out.println("ID: " + id);
+            System.out.println("RUT: " + rut);
+            if (veredicto) {
+                System.out.println("Veredicto: A favor");
+            } else {
+                System.out.println("Veredicto: En contra");
+            }
+        } else {
+            System.out.println("Voto no encontrado para el ID: " + id);
+        }
+    }
+
+        public void modificarVoto(){
         Scanner lector = new Scanner(System.in);
         int opcion;
     do {
@@ -61,7 +94,10 @@ public class Voto {
         System.out.println("¿Qué desea modificar del voto actual?");
         System.out.println("0. Salir\n 1. ID 2. Rut 3. Veredicto");
 
-        opcion = lector.nextInt();
+        //Se lee con escaner y se guarda en la variable opcion  
+        opcion = lector.nextInt(); 
+        
+        // Se utiliza nextLine(Lee un string) para poder leer el '\n' faltante ya que este se deja al leer el nexInt
         lector.nextLine();
 
         switch (opcion) {
