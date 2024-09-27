2f2caae4f5ee626ced7e2bc7d709e93cfe885b5a
BreepaYeah <146591637+Cboyeah@users.noreply.github.com>
2024-09-26 23:05:42 -0300

    Creación del programa Sistema de gestión de consultas
    Este programa esta de forma mas completa que su anterior versión, facilitando con la ejecución de este. Además se agregaron mas funcionalidades como ventanas al programa, también se planea agregar manejo de excepciones especificas(Crear excepcion y usar bloque try-catch), herencia (uso de sobreescritura),etc.
    
    Cabe recalcar que esta versión fue creada en NetBeans IDE 21, por lo que en versiones mas recientes pueden existir errores.

---
+++ Sistema2/src/main/java/com/mycompany/sistema/Consulta.java
@@ -0,0 +1,179 @@
+package com.mycompany.sistema;
+
+import java.util.HashMap;
+
+public class Consulta {
+
+    private int id;
+    private String nombre;
+    private String fecha;
+    private int cantDeVotos;
+    public HashMap<String, Voto> votos;
+    
+
+    //=========== Builder ==============\\
+    
+    public Consulta(int id, String nombre, String fecha){
+        this.id = id;
+        this.nombre = nombre;
+        this.fecha = fecha;
+        this.cantDeVotos = 0;
+        this.votos = new HashMap<String,Voto>();
+    }
+     
+    //=========== Getters ==============\\
+    public int getId(){
+        return id;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public int getCantDeVotos() {
+        return cantDeVotos;
+    }
+
+    public HashMap<String, Voto> getVotos() {
+        return votos;
+    }
+
+    //===========Setters==============\\
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public void setCantDeVotos(int cantDeVotos) {
+        this.cantDeVotos = cantDeVotos;
+    }
+
+    public void setVotos(HashMap<String,Voto> votos) {
+        this.votos = votos;
+    }
+
+    //=========== Métodos ==============\\
+    
+
+
+    public boolean verificarVoto(Voto voto){
+        Voto votoAnterior = votos.put(voto.getRut(), voto);
+
+        if(votoAnterior == null){
+            cantDeVotos++;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean agregarVoto(int id, String rut, boolean veredicto){
+        Voto nuevoVoto = new Voto();
+        nuevoVoto.setId(id);
+        nuevoVoto.setRut(rut);
+        nuevoVoto.setveredicto(veredicto);
+        
+        boolean agregado = verificarVoto(nuevoVoto);
+
+        return agregado;
+    }
+    
+    public Voto obtenerVoto(int idVoto) {
+        for (String rut : votos.keySet()) {
+            Voto voto = votos.get(rut);
+            if (voto.getId() == idVoto) {
+                return voto;  // Retorna el voto si el id coincide
+            }
+        }
+        return null;  // Retorna null si no se encuentra el voto
+    }
+
+    public Voto obtenerVoto(String rut){
+        Voto votoRevision = new Voto();
+        votoRevision.setRut(rut);
+        return votos.get(String.valueOf(rut));
+    }
+
+    public void mostrarVoto(int idVoto){
+        Voto votoMostrar = obtenerVoto(idVoto);
+
+        if(votoMostrar != null){
+            System.out.println("Id Voto : " + votoMostrar.getId());
+            System.out.println("Rut Votante : " + votoMostrar.getRut());
+
+            if(votoMostrar.getveredicto()) {
+                System.out.println("Veredicto : Apruebo");
+            }else{
+                System.out.println("Veredicto : Rechazo");
+            }
+        }else{
+            System.out.println("El id ingresado no existe");
+        }
+    }
+
+    public void mostrarVoto(String rut){
+        Voto votoMostrar2 = obtenerVoto(rut);
+        if(votoMostrar2 != null){
+            System.out.println("Id Voto : " + votoMostrar2.getId());
+            System.out.println("Rut Votante : " + votoMostrar2.getRut());
+
+            if(votoMostrar2.getveredicto()) {
+                System.out.println("Veredicto : Apruebo");
+            }else{
+                System.out.println("Veredicto : Rechazo");
+            }
+        }else{
+
+        }
+    }
+
+    public int contarVotos(){
+        return votos.size();
+    }
+    
+    public void mostrarConsulta(){
+        System.out.println("Consulta ID: " + id);
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Fecha: " + fecha);
+        if(votos.isEmpty()){
+            System.out.println("No hay votos disponibles");
+        }
+        else{
+            System.out.println("Cantidad de Votos: " + cantDeVotos);
+    
+        }
+
+    }
+
+    public void listarVotos() {
+        if (votos.isEmpty()) {
+            System.out.println("No hay votos registrados.");
+        } else {
+            for (String key : votos.keySet()) {  
+                Voto voto = votos.get(key);       
+                
+                System.out.println("Id Voto : " + voto.getId());
+                System.out.println("Rut Votante : " + voto.getRut());
+                
+                if (voto.getveredicto()) {
+                    System.out.println("Veredicto : Apruebo");
+                } else {
+                    System.out.println("Veredicto : Rechazo");
+                }
+                System.out.println();  
+            }
+        }
+    }
+
+    // PROXIMAMENTE METODOS ELIMINAR \\
+}
